c74189377b7cbddf7b9bc4512b39226a8082d19c
unknown <jhonatan.diias@hotmail.com>
2022-10-16 21:12:11 -0400

    Estrutura de Dados

---
+++ Python_para_Cientistas_de_Dados/Estruturas de dados Python/01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -0,0 +1,7 @@
+lista = [1, "Python", [40, 30, 20]]
+
+l2 = lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+print(id(l2),id(lista))
---
+++ Python_para_Cientistas_de_Dados/Estruturas de dados Python/01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -0,0 +1,14 @@
+salario = 2000
+
+
+def salario_bonus(bonus):
+    global salario
+    salario += bonus
+    return salario
+    
+
+
+
+salario_bonus(500)  # 2500
+
+print(salario + salario_bonus)
